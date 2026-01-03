5d08f618e13254daaf549b9a2fc8777948202a28
faridhakanda <faridakanda1729@gmail.com>
2026-01-03 14:23:03 +0600

    after resolve all issue

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,10 @@ function About() {
   return (
     <div>
         <h1>About page for my portfolio project!</h1>
+        <p>Hi there, I am Farid Akanda. I am a full stack developer. Currently I am working with nextjs for frontend and django for backend.</p>
         <Link href="/">Go to Home</Link>
+        <h1>Farid Akanda</h1>
+        <h2>I am a full stack developer!</h2>
     </div>
   )
 }
