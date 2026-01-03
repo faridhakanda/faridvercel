c55bc84ee1120ac80063e78155334850bd7a01c9
faridhakanda <faridakanda1729@gmail.com>
2026-01-03 14:06:09 +0600

    all file commit Jan 3, 2026

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+function About() {
+  return (
+    <div>
+        <h1>About page for my portfolio project!</h1>
+        <Link href="/">Go to Home</Link>
+    </div>
+  )
+}
+
+export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1>Farid Akanda</h1>
      <p>Hello there, I am a computer programmer and full stack developer interested with machine learning, cyber security and distributed system.</p>
+    <Link href="about">About</Link>
     </main>
   );
 }
