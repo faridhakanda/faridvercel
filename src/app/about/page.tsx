adc53253c1d02a95dd6a2c0b4a6784d0d6fc97d0
faridhakanda <faridakanda1729@gmail.com>
2026-02-09 16:10:58 +0600

    update and add about file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 function About() {
   return (
     <div>
-        <h1>About page for my portfolio project!</h1>
-        <p>Hi there, I am Farid Akanda. I am a full stack developer. Currently I am working with nextjs for frontend and django for backend.</p>
+        <h1 className='header-title'>About page for my portfolio project!</h1>
+        <p className='main-title'>Hi there, I am Farid Akanda. I am a full stack developer. Currently I am working with nextjs for frontend and django for backend.</p>
         <Link href="/">Go to Home</Link>
-        <h1>Farid Akanda</h1>
-        <h2>I am a full stack developer!</h2>
+        <h1 className='main-title'>Farid Akanda</h1>
+        <h2 className="secondary-title">I am a full stack developer!</h2>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1>Farid Akanda</h1>
+     <h2 className="header-title">I'm a full stack developer.</h2>
      <p>Hello there, I am a computer programmer and full stack developer interested with machine learning, cyber security and distributed system.</p>
     <Link href="about">About</Link>
     </main>
